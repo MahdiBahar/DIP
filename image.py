ccce7c2180d075e58c09372ec7abea04567fb59f
Mahdi Bahar <mahdi.bahar.g@gmail.com>
2023-03-27 13:56:55 +0430

    'import'

---
+++ image.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 27 12:29:02 2023
+
+@author: ASUS
+"""
+## import part
+
+import imageio
+
+#opencv
+import cv2 as cv
+
+#scikit image
+import skimage
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+#pilow
+from PIL import Image
+
+
+#reading image
+
+img_one = cv.imread( )
+
+
+
+ 
