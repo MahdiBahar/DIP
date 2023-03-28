2bb7f86cefd29c247e15abd1a44bedc92dc5a0cf
Mahdi Bahar <mahdi.bahar.g@gmail.com>
2023-03-28 16:51:48 +0430

    Color Image Channels

--- image.py
+++ image.py
@@ -52,9 +52,57 @@ img_two_cv_gray = cv.imread(r'dataset/images/b.jpg',0)
 plt.imshow(img_two_cv_gray)
 print('shape of gray image',img_two_cv_gray.shape)
 
+#color
+img_two_cv_color = cv.imread(r'dataset/images/b.jpg',1)
+plt.imshow(img_two_cv_color)
+print('shape of color image',img_two_cv_color.shape)
+
+#unchange
+img_two_cv_unchange = cv.imread(r'dataset/images/b.jpg',-1)
+plt.imshow(img_two_cv_unchange)
+print('shape of unchange image',img_two_cv_unchange.shape)
+
 
 #color to Gray 
 img_two_cv = cv.imread(r'dataset/images/b.jpg')
 img_2_cv_gray = cv.cvtColor(img_two_cv,cv.COLOR_RGB2GRAY)
 cv.imshow('image cv 2 gray',img_2_cv_gray)
 cv.waitKey(0)
+
+#color image channnels
+
+img_channels = cv.imread(r'dataset/images/b.jpg')
+
+#bgr
+blue_ch = img_channels[:,:,0]
+green_ch = img_channels[:,:,1]
+red_ch = img_channels[:,:,2]
+
+plt.subplot(2,2,1)
+plt.imshow(img_channels)
+plt.title("orginal image")
+plt.axis('off')
+
+
+plt.subplot(2,2,2)
+plt.imshow(blue_ch)
+plt.title("blue ch")
+plt.axis('off')
+
+
+
+plt.subplot(2,2,3)
+plt.imshow(green_ch)
+plt.title("green ch")
+plt.axis('off')
+ 
+
+plt.subplot(2,2,4)
+plt.imshow(red_ch)
+plt.title("red ch")
+plt.axis('off')
+
+
+
+
+
